610699f6c8b490700a9d0fd7d696fbf03c4cdc58
djp952 <djp952@gmail.com>
2017-12-23 18:52:44 -0500

    Refactor MSBUILD project to allow for smaller builds; add target osx-x86_64

--- Application.mk
+++ Application.mk
@@ -20,7 +20,6 @@
 # SOFTWARE.
 #-----------------------------------------------------------------------------
 
-APP_ABI := armeabi-v7a x86
 APP_BUILD_SCRIPT := Android.mk
 APP_PIE := true
 APP_PLATFORM := android-21
