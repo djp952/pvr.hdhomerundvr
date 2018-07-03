4a7312bb5b2ab00ca7d1acb25a1d1c3db9668fd3
djp952 <djp952@gmail.com>
2018-07-03 00:09:29 -0400

    Update copyright date to 2018

--- src/stdafx.h
+++ src/stdafx.h
@@ -1,5 +1,5 @@
 //---------------------------------------------------------------------------
-// Copyright (c) 2017 Michael G. Brehm
+// Copyright (c) 2018 Michael G. Brehm
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
